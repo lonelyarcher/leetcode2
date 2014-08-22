70e614d38955f18e17284b342ab596134ad5ade3
Meng Xu <xumeng@gmail.com>
2014-08-22 19:15:12 -0400

    ..

--- src/leetcode/RegularExpressionMatching.java
+++ src/leetcode/RegularExpressionMatching.java
@@ -4,31 +4,31 @@ public class RegularExpressionMatching {
 
 	public static void main(String[] args) {
 		RegularExpressionMatching rem = new RegularExpressionMatching();
-		System.out.println(rem.isEmpty("c*a*b*",0));
-		System.out.println(rem.isMatch("aaab","c*a*b"));
+		//System.out.println(rem.isEmpty("c*a*b*",0));
+		System.out.println(rem.isMatch("aa",".*"));
 
 	}
 	
 	public boolean isMatch(String s, String p) {
 		if(s==null || p==null) return false;
+		
 		return isMatch(s,0,p,0);
 	}
 
 	private boolean isMatch(String s, int sp, String p, int pp) {
-		
 		if(p.length()==0) return s.length()==0;
-		
 		char preP = '\u0000';
 		while(sp<s.length()){
 			if(pp>=p.length()) return false;
-			else if(p.charAt(pp)=='.' ) return isMatch(s,sp+1,p,pp+1);
+			if(pp>0) preP = p.charAt(pp-1);
+			if(pp<p.length()-1 && p.charAt(pp+1)=='*' && isMatch(s,sp,p,pp+2)) return true;
+			if(p.charAt(pp)=='.' ) return isMatch(s,sp+1,p,pp+1);
 			else if(s.charAt(sp)==p.charAt(pp))  return isMatch(s,sp+1,p,pp+1);
 			else if(p.charAt(pp)=='*'){
-				if(s.charAt(sp)==preP) return isMatch(s,sp,p,pp+1);
-				else return isMatch(s.substring(sp),p.substring(pp+1));
+				if(s.charAt(sp)==preP || preP=='.') return isMatch(s,sp+1,p,pp) || isMatch(s,sp,p,pp+1);
+				else return isMatch(s,sp,p,pp+1);
 			}
-		
-			else if(sp<s.length()-1 && s.charAt(sp+1)=='*') return isMatch(s,sp,p,pp+2);
+			else return false;
 		}
 		return isEmpty(p,pp);
 	}
